f3d1034ba7f2fd8cf9b21c0d434123344dc18b30
Chris Gallagher <cgally103@gmail.com>
2018-03-13 12:19:20 -0400

    complete assignment 5

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,29 +2,69 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
   end
 
   def []=(key, value)
+    item = Node.new(key, value)
+    i = index(key, size)
+    if @items[i] == nil
+      @items[i] = item
+    elsif @items[i].key != key
+      self.resize
+      self[key] = value
+    elsif @items[i].key == key && @items[i].value != value
+      if self.next_open_index(i) == -1
+        self.resize
+        @items[i] = item
+      else
+        @items[i] = item
+      end
+    end
   end
 
   def [](key)
+    if @items[index(key, size)] != nil && @items[index(key, size)].key == key
+      @items[index(key, size)].value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    hash_code = 0
+    key.each_byte do |c|
+      hash_code = hash_code + c
+    end
+    hash_code % size
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    while index < @items.length - 1
+      if @items[index] == nil
+        return index
+      else
+        index += 1
+      end
+    end
+    return -1
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    temp = @items
+    @items = Array.new(@items.length * 2)
+    temp.each do |item|
+      if item != nil
+        self[item.key] = item.value
+      end
+    end
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,29 +5,101 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
   end
 
   def []=(key, value)
-  end
+    item = Node.new(key,value)
+    i = index(key,size)
+    if @items[i] == nil
+      @items[i] = LinkedList.new
+      @items[i].add_to_tail(item)
+    else
+      @items[i].add_to_tail(item)
+    end
+      if load_factor > @max_load_factor
+        self.resize
+      end
+   end
+
 
   def [](key)
+    i = index(key,size)
+    current = @items[i].head
+    if current.key === key
+      current.value
+    else
+      while current.key != key
+        current = current.next
+      end
+      current.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    hash_code = 0
+    key.each_byte do |c|
+      hash_code = hash_code + c
+    end
+    hash_code % size
   end
 
   # Calculate the current load factor
   def load_factor
+    count = 0.0
+    @items.each do |item|
+      if item
+        count += 1
+        current = item.head
+        while current.next
+          count += 1
+          current = current.next
+        end
+      end
+    end
+    count/@items.size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    temp = @items
+    @items = Array.new(@items.length * 2)
+
+    temp.each do |item|
+      if item
+        current = item.head
+        while current
+          i = index(current.key, size)
+          if @items[i] == nil
+            @items[i] = LinkedList.new
+          end
+          @items[i].add_to_tail(current)
+          current = current.next
+        end
+      end
+    end
+  end
+
+  def print_state
+    puts "Load factor: #{load_factor}"
+    puts "{"
+    @items.each_with_index do |item, index|
+      if item != nil
+        current = item.head
+        while current != nil
+          puts "Bucket: #{index}\t#{current.key}:  #{current.value},"
+          current = current.next
+        end
+      end
+    end
+    puts "}"
   end
 end
